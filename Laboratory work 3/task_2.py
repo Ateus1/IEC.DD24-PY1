36120972f6164fe9f857837329b6bcafb70336c0
Ateus1 <104033484+Ateus1@users.noreply.github.com>
2024-11-08 10:24:39 +0300

    Update task_2.py

--- Laboratory work 3/task_2.py
+++ Laboratory work 3/task_2.py
@@ -1,4 +1,4 @@
-def find_common_participants(first_group, second_group, sep = ","):
+def find_common_participants(first_group, second_group, sep=","):
     common_participants = list(set(first_group.split(sep)).intersection(second_group.split(sep)))
     common_participants.sort()
     return common_participants
@@ -6,5 +6,4 @@ def find_common_participants(first_group, second_group, sep = ","):
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-
 print(find_common_participants(participants_first_group, participants_second_group, "|"))
