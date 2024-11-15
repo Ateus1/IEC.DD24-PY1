cf2b7992c374647ee7f21741ea119bef288e811d
Ateus1 <104033484+Ateus1@users.noreply.github.com>
2024-11-15 22:40:56 +0300

    Update task_1.py

--- Laboratory work 3/task_1.py
+++ Laboratory work 3/task_1.py
@@ -1,15 +1,15 @@
 def find_index(items, desired_item):
-    for index, item in enumerate(items):
-        if item == desired_item:
-            return index
-    return "not found"
+    if desired_item in items:
+        return items.index(desired_item)
+    else:
+        return None
 
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
 for find_item in ['банан', 'груша', 'персик']:
     index_item = find_index(items_list, find_item)
-    if index_item != "not found":
+    if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
         print(f"Товар '{find_item}' не найден в списке.")
